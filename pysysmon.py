b728c31a5c0921742c5ef0aa7bcb908052f3ec78
camdoherty <137129607+camdoherty@users.noreply.github.com>
2023-07-14 12:03:07 -0400

    Update pysysmon.py

--- pysysmon.py
+++ pysysmon.py
@@ -18,14 +18,20 @@ mem_usage = mem.percent
 # CPU temperature
 temp = psutil.sensors_temperatures()['coretemp'][0].current
 
+# themal sensors
+tsense = sensors
+
+# create html file with a table displaying system info
+
 with open('pysysmon.html', 'w') as f:
     f.write('<table>\n')
     f.write('<tr><th>Parameter</th><th>Value</th></tr>\n')
     f.write(f'<tr><td>Disk Usage %</td><td>{disk_usage}</td></tr>\n')
     f.write(f'<tr><td>CPU Usage %</td><td>{cpu_usage}</td></tr>\n')
-    f.write(f'<tr><td>Memory Utilization %</td><td>{mem_usage}</td></tr>\n')
+    f.write(f'<tr><td>Memory Usage %</td><td>{mem_usage}</td></tr>\n')
     f.write(f'<tr><td>CPU Temperature</td><td>{temp}</td></tr>\n')
     f.write(f'<tr><td>Network In Bytes</td><td>{net_in_bytes:.2f}</td></tr>\n')
     f.write(f'<tr><td>Network Out Bytes</td><td>{net_out_bytes:.2f}</td></tr>\n')
+    f.write(f'<tr><td>Thermal sensors</td><td>{tsense}</td></tr>\n')
     f.write('</table>')
     
